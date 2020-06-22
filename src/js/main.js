a42a2bf3d0e525f47ceca9565c9d0fe1f5c92a69
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-06-22 17:28:13 +0200

    Disconnect and reconnect device.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 
-const connectBtn = document.querySelector('.btn-connect');
-connectBtn.addEventListener('click', requestDevice);
+const scanBtn = document.querySelector('.btn-scan');
+const disconnectBtn = document.querySelector('.btn-disconnect');
+const reconnectBtn = document.querySelector('.btn-reconnect');
+scanBtn.addEventListener('click', requestDevice);
+disconnectBtn.addEventListener('click', disconnectDevice);
+reconnectBtn.addEventListener('click', reconnectDevice);
 
 let device;
 
 async function requestDevice() {
-	console.log('connect');
 	const options = { 
 		filters: [{
 			namePrefix: 'BlenoService',
@@ -15,6 +18,7 @@ async function requestDevice() {
     console.log('Requesting Bluetooth Device...');
     console.log('with ' + JSON.stringify(options));
 		device = await navigator.bluetooth.requestDevice(options);
+		device.addEventListener('gattserverdisconnected', onDisconnected);
     console.log('> Name:             ' + device.name);
     console.log('> Id:               ' + device.id);
 		console.log('> Connected:        ' + device.gatt.connected);
@@ -30,4 +34,36 @@ async function connectDevice() {
 	console.log('Connecting to Bluetooth Device...');
   await device.gatt.connect();
   console.log('> Bluetooth Device connected');
+}
+
+function disconnectDevice() {
+  if (!device) {
+    return;
+  }
+  console.log('Disconnecting from Bluetooth Device...');
+  if (device.gatt.connected) {
+    device.gatt.disconnect();
+  } else {
+    console.log('> Bluetooth Device is already disconnected');
+  }
+}
+
+function onDisconnected(event) {
+  // Object event.target is Bluetooth Device getting disconnected.
+  console.log('> Bluetooth Device disconnected');
+}
+
+function reconnectDevice() {
+  if (!device) {
+    return;
+  }
+  if (device.gatt.connected) {
+    console.log('> Bluetooth Device is already connected');
+    return;
+  }
+  try {
+    connectDevice();
+  } catch(error) {
+    console.log('Oh no! ' + error);
+  }
 }
