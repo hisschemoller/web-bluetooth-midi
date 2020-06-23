f482c3e804c18c3f30c36ad579f70b8b08332a9b
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-06-23 17:28:18 +0200

    Get the device's services.

--- src/js/main.js
+++ src/js/main.js
@@ -6,14 +6,16 @@ scanBtn.addEventListener('click', requestDevice);
 disconnectBtn.addEventListener('click', disconnectDevice);
 reconnectBtn.addEventListener('click', reconnectDevice);
 
-let device;
+const bluetoothServiceUUID = '27cf08c1-076a-41af-becd-02ed6f6109b9';
+let device, server, service;
 
 async function requestDevice() {
 	const options = { 
 		filters: [{
 			namePrefix: 'BlenoService',
-		} 
-	]};
+    }],
+    optionalServices: [ bluetoothServiceUUID ],
+  };
 	try {
     console.log('Requesting Bluetooth Device...');
     console.log('with ' + JSON.stringify(options));
@@ -25,15 +27,18 @@ async function requestDevice() {
 		if (!device.gatt.connected) {
 			connectDevice();
 		}
-  } catch(error)  {
+  } catch (error)  {
     console.log('No! ' + error);
   }
 }
 
 async function connectDevice() {
 	console.log('Connecting to Bluetooth Device...');
-  await device.gatt.connect();
+  server = await device.gatt.connect();
+  console.log('> gatt', device.gatt);
+  console.log('> server', server);
   console.log('> Bluetooth Device connected');
+  getService();
 }
 
 function disconnectDevice() {
@@ -47,6 +52,13 @@ function disconnectDevice() {
     console.log('> Bluetooth Device is already disconnected');
   }
 }
+
+async function getService() {
+  const services = await server.getPrimaryServices();
+  console.log('> Bluetooth services', services);
+  service = await server.getPrimaryService(bluetoothServiceUUID);
+  console.log('> Bluetooth service', service);
+};
 
 function onDisconnected(event) {
   // Object event.target is Bluetooth Device getting disconnected.
